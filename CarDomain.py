b715a8e59c5345914064ca4e63f79fd55249c034
YueyOare <dracoshabond15@gmail.com>
2023-12-13 21:01:33 +0300

    add documentation and some new methods

--- CarDomain.py
+++ CarDomain.py
@@ -1,30 +1,109 @@
-class Car:
-    def __init__(self, carId):
-        self.carId = None
-        self.carNumber = None
-        self.carModel = None
-        self.rentPrice = None
-        self.rentStatus = None
-        self.carType = None
-        self.documents = None
-
-    def getId(self):
-        return self.carId
-
-    def setRentStatus(self, newRentStatus):
-        self.rentStatus = newRentStatus
-
-    def addDocument(self, document):
-        self.documents.append(document)
+from typing import Optional
+import copy
 
 
 class CarDocument:
-    def __init__(self, carId):
-        self.carId = carId
-        self.documentLocation = None
+    def __init__(self, carId: int) -> None:
+        """
+        Initializes the CarDocument object with a car ID.
 
-    def getCarId(self):
+        Args:
+            carId (int): Car ID.
+        """
+        self.carId: int = carId
+        self.documentLocation: str | None = None
+
+    def get_car_id(self) -> int:
+        """
+        Gets the car ID associated with the document.
+
+        Returns:
+            int: Car ID.
+        """
         return self.carId
 
-    def getDocumentLocation(self):
+    def get_document_location(self) -> str | None:
+        """
+        Gets the location of the document.
+
+        Returns:
+            str | None: Document location.
+        """
         return self.documentLocation
+
+
+class Car:
+    def __init__(self, carId: int, carNumber: str | None = None, carModel: str | None = None,
+                 rentPrice: float | None = None, rentStatus: bool | None = None,
+                 carType: str | None = None, documents: list[CarDocument] | None = None) -> None:
+        """
+        Initializes the Car object with optional parameters.
+
+        Args:
+            carId (int): The unique identifier for the car.
+            carNumber (str | None): Car registration number.
+            carModel (str | None): Car model.
+            rentPrice (float | None): Rental price per day.
+            rentStatus (bool | None): Rental status of the car.
+            carType (str | None): Type or category of the car.
+            documents (list[CarDocument] | None): List of documents associated with the car.
+
+        Note:
+            If `carNumber`, `carModel`, `rentPrice`, `rentStatus`, `carType`, and `documents` are not provided,
+            they will be loaded from the database automatically.
+            Otherwise, if provided, they will be uploaded to the database automatically.
+        """
+        self.carId: int = carId
+        self.carNumber: str | None = carNumber
+        self.carModel: str | None = carModel
+        self.rentPrice: float | None = rentPrice
+        self.rentStatus: bool | None = rentStatus
+        self.carType: str | None = carType
+        self.documents: list[CarDocument] | None = documents
+
+        if carNumber is None and carModel is None and rentPrice is None \
+                and rentStatus is None and carType is None and documents is None:
+            self.load_car_info()
+        else:
+            self.upload_car_info()
+
+    def upload_car_info(self) -> None:
+        """
+        Uploads car information to the database.
+        """
+        # Placeholder for actual implementation
+
+    def load_car_info(self) -> None:
+        """
+        Loads car information from the database.
+        """
+        if self.carId is not None:
+            # Placeholder for actual implementation
+            pass
+
+    def get_id(self) -> int:
+        """
+        Gets the unique identifier for the car.
+
+        Returns:
+            int: Car ID.
+        """
+        return self.carId
+
+    def set_rent_status(self, new_rent_status: bool) -> None:
+        """
+        Sets the rental status of the car.
+
+        Args:
+            new_rent_status (bool): New rental status.
+        """
+        self.rentStatus = new_rent_status
+
+    def add_document(self, document: CarDocument) -> None:
+        """
+        Adds a document to the list of documents associated with the car.
+
+        Args:
+            document (CarDocument): Document object to be added.
+        """
+        self.documents.append(document)
---
+++ Users.py
@@ -0,0 +1,324 @@
+from typing import Optional, Dict
+import copy
+from datetime import datetime
+
+
+class BaseInfo:
+    def __init__(self, id: int, firstName: str | None = None,
+                 lastName: str | None = None, birthDate: datetime | None = None) -> None:
+        """
+        Initializes the BaseInfo object with optional parameters.
+
+        Args:
+            id (int): The user ID.
+            firstName (str | None): First name of the user.
+            lastName (str | None): Last name of the user.
+            birthDate (datetime | None): Birthdate of the user.
+
+        Note:
+            If `firstName`, `lastName` and `birthDate` aren't provided, they will be loaded from the database automatically.
+            In other case, they will be added to database automatically.
+        """
+        self.id: int = id
+        self.firstName: str | None = firstName
+        self.lastName: str | None = lastName
+        self.birthDate: datetime | None = birthDate
+        if id is None:
+            self.load_base_info()
+        else:
+            self.upload_base_info()
+
+    def upload_base_info(self) -> None:
+        """
+        Uploads user's base information to the database.
+        """
+        # Placeholder for actual implementation
+
+    def load_base_info(self) -> None:
+        """
+        Loads user's base information from the database.
+        """
+        if self.id is not None:
+            # Placeholder for actual implementation
+            pass
+
+
+class Credential:
+    def __init__(self, login: str, password: str) -> None:
+        """
+        Initializes the Credential object with login and password.
+
+        Args:
+            login (str): User's login.
+            password (str): Password which user writes when logging in.
+        """
+        self.login: str = login
+        self.password_to_validate: str = password
+
+    def get_id(self) -> int | None:
+        """
+        Gets the user ID if the password is valid.
+
+        Returns:
+            int | None: User ID or None if the password is invalid.
+        """
+        if self.__to_valid_password():
+            # Placeholder for actual implementation
+            pass
+        else:
+            return None
+
+    def __get_password(self) -> str:
+        """
+        Retrieves the password from the database based on the login.
+
+        Returns:
+            str: User's password.
+        """
+        # Placeholder for actual implementation
+        pass
+
+    def __to_valid_password(self) -> bool:
+        """
+        Validates if the provided password matches the stored password.
+
+        Returns:
+            bool: True if the password is valid, False otherwise.
+        """
+        return self.__get_password() == self.password_to_validate
+
+    def update_login(self, new_login: str) -> None:
+        """
+        Updates the user's login and reflects the change in the database.
+
+        Args:
+            new_login (str): New login for the user.
+        """
+        old_login: str = self.login
+        self.login = new_login
+        # Placeholder for actual implementation
+
+    def update_password(self, new_password: str) -> None:
+        """
+        Updates the user's password and reflects the change in the database.
+
+        Args:
+            new_password (str): New password for the user.
+        """
+        # Placeholder for actual implementation
+
+
+class PrivateInfo:
+    def __init__(self, id: int | None = None, privateInfo: Dict[str, str | None] = None) -> None:
+        """
+        Initializes the PrivateInfo object with a user ID.
+
+        Args:
+            id (int): User ID. If not provided, upload given privateInfo to database by method `upload_private_info`
+            and get id for new user
+        """
+        self.id: int = id
+        if id is None:
+            self.id = self.upload_private_info(privateInfo)
+
+    def get_private_info(self) -> Dict[str, str | None]:
+        """
+        Retrieves private information of the user from the database.
+
+        Returns:
+            Dict[str, str | None]: Dictionary containing private information.
+        """
+        info: Dict[str, str | None] = None
+        return copy.deepcopy(info)
+
+    def upload_private_info(self, args: Dict[str, str | None]) -> int:
+        """
+        Uploads private information of the user to the database.
+        if self.id isn't specified, gets it from database
+
+        Args:
+            args (Dict[str, str | None]): Dictionary containing private information.
+
+        Returns:
+            int: User id
+        """
+        # Placeholder for actual implementation
+        # if self.id is None (new user), get a new id
+        del args
+        return self.id
+
+
+class User:
+    def __init__(self, id: int | None = None) -> None:
+        """
+        Initializes the User object with optional parameters.
+
+        Args:
+            id (int | None): User ID. Can be an integer if the ID is already known,
+                            or None if it is unknown (next action must be `enter_system`).
+        """
+        self.id: int | None = id
+        self.baseInfo: BaseInfo | None = None
+        self.load_base_info()
+        self.privateInfo: PrivateInfo | None = None
+        self.load_private_info()
+        self.creds: Credential | None = None
+
+    def enter_system(self, login: str, password: str) -> None:
+        """
+        Enters the system by validating user credentials and loads information.
+
+        Args:
+            login (str): User login.
+            password (str): Entered password.
+        """
+        self.creds = Credential(login, password)
+        id: int | None = self.creds.get_id()
+        if id is not None:
+            self.id = id
+            self.load_private_info()
+            self.load_base_info()
+
+    def load_private_info(self) -> None:
+        """
+        Loads user's private information from the database.
+        """
+        if self.id is not None:
+            self.privateInfo = PrivateInfo(self.id)
+        else:
+            self.privateInfo = None
+
+    def load_base_info(self) -> None:
+        """
+        Loads user's base information from the database.
+        """
+        if self.id is not None:
+            self.baseInfo = BaseInfo(self.id)
+        else:
+            self.baseInfo = None
+
+    def change_base_info(self, new_info: Dict[str, str | None]) -> None:
+        """
+        Changes user's base information and updates it in the database.
+
+        Args:
+            new_info (Dict[str, str | None]): Dictionary containing new base information
+                                               in format {'firstName': str, 'lastName': str, 'birthDate': datetime}.
+        """
+        # Placeholder for actual implementation
+        self.baseInfo.firstName = new_info['firstName']
+        self.baseInfo.lastName = new_info['lastName']
+        self.baseInfo.birthDate = new_info['birthDate']
+        self.baseInfo.upload_base_info()
+
+    def change_private_info(self, new_info: Dict[str, str | None]) -> None:
+        """
+        Changes user's private information and updates it in the database.
+
+        Args:
+            new_info (Dict[str, str | None]): Dictionary containing new private information.
+        """
+        self.privateInfo.upload_private_info(new_info)
+        del new_info
+
+    def change_credentials(self, new_info: Dict[str, str]) -> None:
+        """
+        Changes user's credentials (login and password) and updates them in the database.
+
+        Args:
+            new_info (Dict[str, str]): Dictionary containing new credentials.
+        """
+        self.creds.update_password(new_info['password'])
+        self.creds.update_login(new_info['login'])
+
+
+class Renter(User):
+    def __init__(self) -> None:
+        """
+        Initializes the Renter object with additional attributes.
+        """
+        super().__init__()
+        self.registrationDate: datetime | None = None
+        self.driverLicenseDate: datetime | None = None
+        self.canRent: bool = True
+
+    def enter_system_as_renter(self, login: str, password: str) -> None:
+        """
+        Enters the system as a renter and loads additional information.
+
+        Args:
+            login (str): Renter login.
+            password (str): Renter password.
+        """
+        self.enter_system(login, password)
+        # Placeholder for actual implementation
+
+    def register(self, args: Dict[str, str]) -> None:
+        """
+        Registers a new renter and inserts their information into the database.
+
+        Args:
+            args (Dict[str, str]): Dictionary containing information for registration.
+        """
+        # Placeholder for actual implementation
+        # Insert entered data into tables, don't forget to check login for uniqueness
+        # self.privateInfo = PrivateInfo(privateInfo) from args
+        self.id = self.privateInfo.id
+        # self.baseInfo = BaseInfo(self.id, firstName, lastName, birthDate) from args
+        self.registrationDate = datetime.now().date()
+        # self.driverLicenceDate take from args
+
+    def count_driver_experience(self) -> datetime:
+        """
+        Calculates the driver experience based on the current date and driver's license date.
+
+        Returns:
+            datetime: Driver experience duration.
+        """
+        return datetime.now().date() - self.driverLicenseDate
+
+    def count_time_in_system(self) -> datetime:
+        """
+        Calculates the time spent in the system based on the current date and registration date.
+
+        Returns:
+            datetime: Time in the system duration.
+        """
+        return datetime.now().date() - self.registrationDate
+
+    def get_agreements(self) -> None:
+        """
+        Retrieves agreements from the Agreement document based on the user ID.
+        """
+        pass
+
+    def set_rent_ability(self, val: bool) -> None:
+        """
+        Sets the rent ability status for the renter.
+
+        Args:
+            val (bool): Rental status.
+        """
+        self.canRent = val
+
+
+class CompanyWorker(User):
+    def __init__(self) -> None:
+        """
+        Initializes the CompanyWorker object.
+        """
+        super().__init__()
+        self.position: str | None = None
+        self.isActive: bool | None = None
+
+    def enter_system_as_worker(self, login: str, password: str) -> None:
+        """
+        Enters the system as a company worker and loads additional information.
+
+        Args:
+            login (str): Worker login.
+            password (str): Worker password.
+        """
+        self.enter_system(login, password)
+        # Placeholder for actual implementation
+        # Retrieve position, isActive from the companyWorker table based on id
